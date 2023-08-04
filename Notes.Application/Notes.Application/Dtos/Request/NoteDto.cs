f9d3ad9e8c7d9896c7bc91bf085820143d65f759
Aashik Villa <avilla@frontlineed.com>
2023-08-04 20:36:57 +0530

    added test to check if end point exists

---
+++ Notes.Application/Notes.Application/Dtos/Request/NoteDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Notes.Application.Dtos.Request
+{
+    public class NoteDto
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Priority { get; set; }
+
+        public string Status { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
---
+++ Notes/Controllers/NoteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Notes.Application.Dtos.Request;
+
+namespace Notes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : Controller
+    {
+        [HttpPut("UpdateNote")]
+        public IActionResult UpdateNote(NoteDto noteDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
